41e96f371db481e0afd7c4296ca3eaf7b42b6a12
Vinit Hemadri <vineet.vh@gmail.com>
2022-04-27 12:51:59 +0530

    Styled explore page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 import PrivateRoute from "./components/PrivateRoute";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
+import Category from "./pages/Category";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,41 @@
+import { Link } from "react-router-dom";
+import rentCategoryImg from "../assets/rentCategory.jpg";
+import saleCategoryImg from "../assets/saleCategory.jpg";
+
 function Explore() {
-  return <div>Explore</div>;
+  return (
+    <section>
+      <header className="my-2 mx-12">
+        <h1 className="text-3xl font-bold text-center">Explore</h1>
+      </header>
+
+      <main className="my-8">
+        {/* Slider */}
+        <p className="mx-12 my-2 text-xl font-bold">Categories</p>
+        <div className="container mx-auto w-full grid grid-cols-2">
+          <div>
+            <Link to="/category/rent">
+              <img
+                src={rentCategoryImg}
+                alt="Rent Category"
+                className="w-auto h-4/5 object-cover rounded-3xl mx-auto"
+              />
+            </Link>
+            <p>Places for Rent</p>
+          </div>
+          <div>
+            <Link to="/category/rent">
+              <img
+                src={saleCategoryImg}
+                alt="Sale Category"
+                className="w-auto h-4/5 object-cover rounded-3xl mx-auto"
+              />
+            </Link>
+            <p>Places for Sale</p>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
 }
 export default Explore;
